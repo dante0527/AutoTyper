11c3522ccf23181906317cf5a4166792cb68050c
dante0527 <19dante.j.lee@gmail.com>
2022-10-05 14:34:11 -0400

    restored py_to_txt conversion func

--- autotype.py
+++ autotype.py
@@ -1,9 +1,29 @@
 from pynput.keyboard import Key, Controller
+import os, sys, time
 import typer
-import time
-import os, sys
+import pathlib
+
 
 app = typer.Typer()
+
+# Convert .py to .txt and return text
+def py_to_txt(file):
+
+    # Convert Python script to .txt
+    with open(file, 'r'):
+
+        # Filename stem
+        stem = f"{pathlib.Path(file).stem}"
+
+        # Copy file.py to file.txt
+        os.system(f"cp {file} {stem}.txt")
+
+    # Read text from file.txt
+    with open(f"{stem}.txt", 'r') as ftxt:
+        text = ftxt.read()
+
+    return text
+
 
 # Read file and return text
 def read_file(file):
